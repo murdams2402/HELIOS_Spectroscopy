b6ecf8ee0beba52ab5f1c47ea6c35aa8b60cafe8
DamianMur <80451810+murdams2402@users.noreply.github.com>
2023-07-24 22:13:23 +0200

    Preparation for data

--- doppler_broadening.py
+++ doppler_broadening.py
@@ -1,11 +1,14 @@
 import matplotlib
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
-from utils import Voigt
-from uncertainties import ufloat
+from scipy.signal import peak_widths
+from utils import Voigt, gaussian, lorenzian
+from numpy import sqrt, log
+# from uncertainties import ufloat
 
 import plot_config
 import pandas as pd
+import numpy as np
 
 from datetime import datetime
 from simple_spectrometer import get_background_noise, get_snapshot
@@ -14,9 +17,8 @@ integration_time = 4000
 
 print("Acquiring background noise ... \n ")
 get_background_noise(integration_time)
+background = pd.read_table("background_noise_raw_data.txt", sep=" ", names=["wavelegnth", "intensiy"], skiprows=2)
 
-now = datetime.now()
-dt_string = now.strftime("%d:%m:%Y_%H-%M-%S")
 
 print("Specify valid wavelength range [1 - 2048 nm]: \n")
 start = int(input("Start: "))
@@ -25,17 +27,69 @@ print("\n")
 
 starting_time = datetime.now()
 
+data = pd.DataFrame(columns=["pressure", "moment", "wavelegnth", "intensiy"])
+
 pressure = 1
+moments = []
+moments.append(starting_time)
+
 while pressure > 1e-8:
-    pressure = input("Enter pressure value [mbar]: p = ")
+    pressure = input("Enter actual pressure value [mbar]: p = ")
     now = datetime.now()
-    get_snapshot(integration_time=integration_time, name=now, start=start, end=end)
+    dt_string = now.strftime("%d:%m:%y_%H-%M-%S")
+
+    # Getting data: measuring plasma spectrum 
+    get_snapshot(integration_time=integration_time, name="doppler_broadening_data_" + dt_string, start=start, end=end)
+    
+    # Importing data 
+    brut = pd.read_table(f"doppler_broadening_data\_pressure={pressure}mbar\_time=" + dt_string, 
+                         sep=" ", 
+                         names=["wavelegnth", "intensiy"], 
+                         skiprows=2)
+    raw = [(pressure, now, lambda_, I) for lambda_, I in zip(brut['wavelegnth'], brut["intensiy"])]
+    data_ = pd.DataFrame(raw, columns=["pressure", "moment", "wavelegnth", "intensiy"])
+    data_["intensiy"] = data_["intensiy"] - background["intensiy"][start-1:end+1]
+    data = pd.concat([data, data_])
+
+    moments.append(now)
+
     stop = input("Continue? Y/N :")
     if stop == 'Y':
         break
 
-# Impprting data 
-file = input("Enter spectrum data file name (relative file path): ")
-data = pd.read_table(file, sep=" ", names=["wavelegnth", "intensiy"], skiprows=2)
+# Analysing the data : fitting broadening with Gaussian / Lorenzian / Voigt function
+#                      and determining gas temperature
+FWHM_Gauss = []
+FWHM_Lorenzian = []
+FWHM_Voigt = []
+FWHM_Voigt_estimation = []
+for p in data["pressure"]:
+    starting_points = []    
+    # The FWHM of a Lorenzian is two times it's parameter b = \gamma (see function definition in utils.py)
+    popt_Lorenzian, covopt_Lorenzian = curve_fit(lorenzian, 
+                                             data[data["pressure"] == p]["intensiy"], 
+                                             p0=starting_points)
+    FWHM_Lorenzian.append(2*popt_Lorenzian[1])
 
-# 
+    starting_points = []
+    # The FWHM of a Gaussian is equal to FWHM = sigma*sqrt( 8*ln(2) )
+    popt_Gauss, covopt_Gauss = curve_fit(gaussian, 
+                                         data[data["pressure"] == p]["intensiy"], 
+                                         p0=starting_points)
+    sigma = 1/sqrt(2*popt_Gauss[2])
+    Delta_lambda = sigma*sqrt(8*log(2))
+    FWHM_Gauss.append(Delta_lambda)
+    
+    starting_points = []
+    # The FWHM of the Voigt profile function is determined numerically
+    popt_Voigt, covopt_Voigt = curve_fit(Voigt, 
+                                         data[data["pressure"] == p]["intensiy"], 
+                                         p0=starting_points)
+    FWHM_Voigt.append()
+    # However there is an approximation, which we can use as 4th estimation for the temperature
+    FWHM_Voigt_estimation.append(0.5346*2*popt_Lorenzian[1] + sqrt(0.2166*(4*popt_Lorenzian[1]*popt_Lorenzian[1]) + Delta_lambda*Delta_lambda))
+    
+    # indices of peakes
+    peaks = []
+    # Using scipy's function that retreaves the FWHM of the specified peaks
+    widths, _ = peak_widths(x=data[data["pressure"] == p]["intensiy"], peaks=peaks )
--- utils.py
+++ utils.py
@@ -1,13 +1,14 @@
 from numpy import exp, convolve
+from math import pi
 from parse import *
 
 def gaussian(x, a, b, c):
     return a*exp(-(x-b)*(x-b)*c)
 
-def lorenzian(x, b):
-    return 1/(b + x*x)
+def lorenzian(x, x0, b):
+    return (1/pi)*b/(b*b + (x-x0)*(x-x0))
 
-def Voigt(x, a, b):
-    f = gaussian(x, 1, 0, a)
-    g = lorenzian(x, b)
+def Voigt(x, x0, a, b, A):
+    f = gaussian(x, 1, x0, a)
+    g = lorenzian(x, x0, b)
     return convolve(f, g)
