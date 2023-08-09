a7f9ed323b437fa0a184357ed5bada074c0d3b67
DamianMur <80451810+murdams2402@users.noreply.github.com>
2023-08-09 16:17:28 +0200

    ratio continuation

--- line_ratio.py
+++ line_ratio.py
@@ -8,4 +8,5 @@ while True:
     power = float(input("Enter RF power [W] = "))
     fullname = get_snapshot(path='Spectrum_data/Line_ratio/',name=f"gas={gas}_shot={shot}_power={power}W_")
 
-# The Coil power is fixed at 63.8 A for shots from 1 to 25
+# The coil current is fixed at 63.8 A for shots 1 to 25
+# The coil current is fixed at 0 A for shots 26 to 45
--- line_ratio_analysis.py
+++ line_ratio_analysis.py
@@ -3,9 +3,55 @@ import matplotlib.pyplot as plt
 from utils import get_files_and_params
 from scipy.constants import c, k, h, e
 
+import plot_config
+from scipy.signal import find_peaks
 
 if __name__ == '__main__':
-    data = pd.DataFrame(columns=[ "file_name", "pressure", "peak wavelength", "peak intensity", "FWHM Doppler", "temperature"])
+    data = pd.DataFrame(columns=["model", "gas", "shot", "power", "wavelength", "intensity"])
     dir = "Spectrum_data/Line_ratio"
-    files = get_files_and_params(dir, format="gas={gas}_shot={shot}_power={power}W_HR4000_spectrum_raw_data.txt")
+    files = get_files_and_params(dir, format="gas={gas}_shot={shot}_power={power}W_{model}_spectrum_raw_data.txt")
+    peaks = data
+
+    for  file in files:
+        file_name = file["name"]
+        
+        # Opening acquired spectrum
+        spectrum = pd.read_table(f"{dir}/{file_name}", 
+                            sep=" ", 
+                            names=["wavelength", "intensity"], 
+                            skiprows=2)
+        temp = [(file["model"], file["gas"], file["shot"], file["power"], w, I) for w, I in zip(spectrum["wavelength"], spectrum["intensity"])]
+        temp = pd.DataFrame(temp, columns=["model", "gas", "shot", "power", "wavelength", "intensity"])
+        data = pd.concat([data, temp])
+        
+        # Picking the spectrum peaks
+        max_peaks = spectrum.max()
+        peaks_info = find_peaks(x=spectrum["intensity"], height=[1000, max_peaks["intensity"]])
+        # peaks_info[1].get("peak_heights")
+        peaks_ = spectrum.filter(items=peaks_info[0], axis=0)
+        temp = [(file["model"], file["gas"], file["shot"], file["power"], w, I) for w, I in zip(peaks_["wavelength"], peaks_["intensity"])]
+        temp = pd.DataFrame(temp, columns=["model", "gas", "shot", "power", "wavelength", "intensity"])
+        peaks = pd.concat([peaks, temp])
     
+    
+    plt.figure()
+    for file in files:
+        shot = file["shot"]
+        if int(shot) > 25:
+            plt.plot(data[data["shot"]==shot]["wavelength"],data[data["shot"] == shot]["intensity"])
+    plt.xlabel(r"$\lambda \rm \ [nm]$")
+    plt.ylabel(r"$\rm Intensity \ [a.u.]$")
+    plt.grid(True)
+    # plt.show()
+
+
+    plt.figure()
+    for file in files:
+        shot = file["shot"]
+        if int(shot) > 25:
+            plt.scatter(peaks[peaks["shot"]==shot]["wavelength"],peaks[peaks["shot"] == shot]["intensity"])
+    plt.xlabel(r"$\lambda \rm \ [nm]$")
+    plt.ylabel(r"$\rm Intensity \ [a.u.]$")
+    plt.grid(True)
+    
+    plt.show() 
